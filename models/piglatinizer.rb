af9ebaa4d8b515236a5d2c24af57ac63e678c0d9
Ian Vlahovic <banddorkfht7@gmail.com>
2018-07-10 05:52:54 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,9 +6,9 @@ class PigLatinizer
   THREE_LETTER_CONSONANT_CLUSTERS = ["phl", "phr", "scr", "shr", "spl", "spr", "squ", "str", "thr"]
   VOWELS = ["a", "e", "i", "o", "u"]
 
-  # def initialize(user_phrase)
-  #   @user_phrase = user_phrase
-  # end
+  def initialize(user_phrase)
+    @user_phrase = user_phrase
+  end
 
   # def words
   #   user_phrase.split
